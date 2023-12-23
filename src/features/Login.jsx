65e511b790189f59734442dc1fab5edca2c1f550
Miguel <aries_242015@hotmail.com>
2023-12-23 14:34:15 -0500

    feature login finalizado

--- src/features/Login.jsx
+++ src/features/Login.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="page-login">
+      <div className="container-login">
+        <div className="logo">
+          <img src="./assets/logo.png" alt="logo" />
+        </div>
+        <div className="formulario">
+          <form action="">
+            <span>Bienvenido</span>
+            <span>Inicia Sesión para continuar.</span>
+            <div className="input-user">
+              <i class="fa-solid fa-user"></i>
+              <input type="text" placeholder="Usuario" />
+            </div>
+            <div className="input-password">
+              <i class="fa-solid fa-lock"></i>
+              <input type="password" placeholder="Contraseña" />
+            </div>
+            <button className="btn-login" type="submit">
+              Ingresar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
